59914f67f1b53a8ee2fbe5faaaab4680de556f9b
marhor <maria.horoshun@live.com>
2021-07-29 18:20:36 +0700

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DxButtonModule } from 'devextreme-angular';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    DxButtonModule
+  ]
+})
+export class AppRoutingModule { }
